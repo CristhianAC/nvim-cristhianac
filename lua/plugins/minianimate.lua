390b44a7ebd4be3e8324dcb644c4b0a082a98774
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-12-01 01:36:21 +0100

    feat: Update neo-tree.nvim and simplify mini.animate scroll subscroll predicate.

--- lua/plugins/minianimate.lua
+++ lua/plugins/minianimate.lua
@@ -5,25 +5,12 @@ return {
     opts = function()
       local animate = require("mini.animate")
       return {
-        resize = {
-          enable = false, -- Desactivar animación de redimensionado
-        },
+        resize = { enable = false },
         scroll = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }), -- Scroll más rápido
-          subscroll = animate.gen_subscroll.equal({
-            predicate = function(total_scroll)
-              if mouse_scrolled then
-                mouse_scrolled = false
-                return false
-              end
-              return total_scroll > 1
-            end,
-          }),
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({ predicate = function() return true end }),
         },
-        cursor = {
-          -- Animación suave del cursor
-          timing = animate.gen_timing.linear({ duration = 80, unit = "total" }),
-        },
+        cursor = { timing = animate.gen_timing.linear({ duration = 80, unit = "total" }) },
       }
     end,
   },
